ef8e5f6b5b7c3fd849c6672e5c6c6f2ecfa3dd71
Lee HyeonJong <guswhd714@naver.com>
2023-05-09 14:10:51 +0900

    0509실습2

--- js/utils.js
+++ js/utils.js
@@ -33,3 +33,22 @@ function solution() {
   console.log(name);
   alert(name + "님 안녕하세요!");
 }
+
+function solution2() {
+  let number = prompt("입력");
+
+  while (isNaN(number) || number === "" || number === null) {
+    alert("숫자를 입력해주세요");
+    number = prompt("입력");
+  }
+
+  if (number >= 1 && number <= 100) {
+    for(let i=1; i<=100; i++){
+        console.log(i);
+    }
+    alert("완료되었습니다")
+  }else{
+    alert("1이상 100이하를 넣어주세요");
+    solution2();
+  }
+}
